694cec9b7ce2398689a45c95902bd8131768e350
BBK-PiJ-2014-22 <jmaciv02@dcs.bbk.ac.uk>
2017-05-13 10:32:33 +0100

    BNZ Implemented

--- coursework/cw-one/SML/resources/test_bnz.sml
+++ coursework/cw-one/SML/resources/test_bnz.sml
@@ -1,5 +1,6 @@
-L1 lin 10 0
+L1 lin 0 10
 L2 lin 1 1
 L3 sub 0 0 1
 L4 add 2 2 1
 L5 bnz 0 L3
+L6 bnz 2 L10
--- coursework/cw-one/SML/src/sml/BnzInstruction.scala
+++ coursework/cw-one/SML/src/sml/BnzInstruction.scala
@@ -3,21 +3,21 @@ package sml
 class BnzInstruction (label : String, op : String, testReg : Int, newInstruction : String ) 
   extends Instruction(label, op) {
   
-  //Not Implmented... debugging option results
   override def execute(m : Machine) {
     if (m.regs(testReg) != 0) {
       val option = m.prog.find(x => x.hasLabel(newInstruction))
       
       option match {
         case Some(result) => 
-          println(result)
+          val index = m.prog.indexOf(result)
+          m.pc = index
         case None  =>
-          println("None")
+          println("No such instruction: " + newInstruction)
       }
     }
   }
   override def toString(): String = {
-    super.toString + " testing " + testReg + " to branch to " + newInstruction + "/n"
+    super.toString + " testing " + testReg + " to branch to " + newInstruction + "\n"
   }
   
 }
