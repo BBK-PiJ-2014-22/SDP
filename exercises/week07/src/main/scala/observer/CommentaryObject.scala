9b73ef1dd7b706584134af48f4963167a0d703c7
BBK-PiJ-2014-22 <jmaciv02@dcs.bbk.ac.uk>
2017-05-14 16:43:23 +0100

    bug in unsubscribe for observer corrected

--- exercises/week07/src/main/scala/observer/CommentaryObject.scala
+++ exercises/week07/src/main/scala/observer/CommentaryObject.scala
@@ -7,12 +7,15 @@ class CommentaryObject(var subscribers: ListBuffer[Observer], val title: String)
   var desc = ""
   
   def subscribeObserver(observer: Observer) = {
-    println(observer + " subscribes from " + title)
-    subscribers = subscribers :+ observer }
+    println(observer + " subscribes to " + title)
+    subscribers = subscribers :+ observer 
+    println(subscribers.length)
+  }
 
   def unSubscribeObserver(observer: Observer) = {
-    println(observer + " unsubscribes from" + title)
-    subscribers = subscribers.filter(_ == observer)}
+    println(observer + " unsubscribes from " + title)
+    subscribers = subscribers.filter(_ != observer)
+    println(subscribers.length)}
 
   def notifyObservers() = {
     for (i <- subscribers) i.update(desc)
